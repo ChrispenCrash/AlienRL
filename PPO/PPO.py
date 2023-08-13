21b9edca8ba5e542466b2e970336756c09fdbb9d
SpocksBeards <chris.cashman@hotmail.com>
2023-08-13 02:46:39 +0100

    Converted train_mt.py to AlienRL.ipynb

--- PPO/PPO.py
+++ PPO/PPO.py
@@ -8,7 +8,7 @@ device = torch.device('cpu')
 if(torch.cuda.is_available()): 
     device = torch.device('cuda:0') 
     torch.cuda.empty_cache()
-    print("Device set to : " + str(torch.cuda.get_device_name(device)))
+    print("Using device: " + str(torch.cuda.get_device_name(device)))
 
 
 class PPO:
